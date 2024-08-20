59490f361fd6c1a274f1bb20e434166dfce8e5f8
Cristian marrugo <cristian.maba2005@outlook.com>
2024-08-19 21:05:25 -0500

    Primer commit de index

---
+++ Ejemplo.js
@@ -0,0 +1,88 @@
+// Condicionales
+
+// let nombre =prompt("Cual es tu nombre?");
+
+
+// if (nombre == "Cristian") {
+    // alert("Hola "+ nombre);
+// } else if (nombre == "Marrugo") {
+    // alert("El señor " + nombre +" es la monda");
+// }else{
+    // alert("Ninguno");
+// }
+
+
+// Operadores intermedios
+
+// let num1 = 12;
+// let num2 = 24;
+// let num3 = 25;
+// let num4 = 91;
+// let num5 = 92;
+
+// op = (true & false) || (true || false );
+
+// document.write(op);
+
+// > = mayor ;
+// < = menor;
+
+// let valor = 12;
+// let valor2 = 24;
+
+// let resultado = valor < valor2;
+// let resultado2 = valor == valor2
+
+// document.write(valor < valor2 || valor == valor2);
+
+
+// Operadores de asignacion
+
+//let num = 20;
+//let num2 = 19;
+
+//document.write(num <= num2);
+
+/// Concatenacion
+
+///nombre = " Cristian";
+
+///frase2 = `hola, ${nombre} Tu eres la monda mk`; 
+ 
+///document.write(frase2);
+
+
+/// Pruebas con prompt///
+
+///let Nombre = prompt("Como te llamas?");
+///alert("Hola " + Nombre);///
+
+/// La variable const se tiene que inicializar apenas se declara Ej: Const Numero = 24; 
+
+
+// function saludar(nombre){
+
+//     let frase = `Hola ${nombre}, como estas`;
+//     document.write(frase);
+//  }
+ 
+//  saludar("Cristian");
+
+let frase = `Hola ${nombre}, como estas`;
+
+//  const saludar = function(nombre){
+//     let frase = `Hola ${nombre}, como estas`;
+//     document.write(frase);
+
+//  }
+
+
+// funciones flecha 
+
+ const saluda = nombre=> {
+    document.write(frase);
+}
+
+ 
+ 
+ 
